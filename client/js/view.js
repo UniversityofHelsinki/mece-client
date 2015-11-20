f3d9228ca4eb1f85dc954ec94ba008f559c8bad9
ohtu <aleroman@ohtu>
2015-11-20 12:42:24 +0200

    MECE-304: Recent changes commited to mercurial repo.

--- client/js/view.js
+++ client/js/view.js
@@ -1,7 +1,9 @@
-
 meceNotifications.view = (function () {
     // elements containing notifications
-    var contentDiv = document.getElementById("content-div");
+    var contentDiv = document.getElementById("mece-content-div");
+
+    var pollingInterval = contentDiv.getAttribute("pollingInterval") || '4000';
+
     var ul = document.createElement("ul");
     contentDiv.appendChild(ul);
 
@@ -13,25 +15,29 @@ meceNotifications.view = (function () {
         ul.appendChild(li);
     }
 
-    function parseNotifications(notifications){
+    function parseNotifications(notifications) {
         // handle notifications here, now just passing through
         // notifications = ...
         appendMsg(notifications);
     }
 
     // call just once on page load
-    meceNotifications.client.getNotifications().then(function (response) {
-        parseNotifications(response);
-    }, function (error) {
-        console.error("Failed!", error);
-    });
 
-    //// requesting notifications in intervals.
-    //setInterval(function () {
-    //    meceNotifications.client.getNotifications().then(function (response) {
-    //        parseNotifications(response);
-    //    }, function (error) {
-    //        console.error("Failed!", error);
-    //    });
-    //}, 4000);
+     meceNotifications.client.getNotifications().then(function (response) {
+
+     parseNotifications(response);
+     }, function (error) {
+     console.error("Failed!", error);
+     });
+
+    /*
+        //// requesting notifications in intervals.
+        setInterval(function () {
+            meceNotifications.client.getNotifications().then(function (response) {
+                parseNotifications(response);
+            }, function (error) {
+                console.error("Failed!", error);
+            });
+        }, pollingInterval);
+        */
 })();
