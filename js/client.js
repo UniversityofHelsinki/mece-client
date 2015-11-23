8c2092e441064c60eb000736eedbb239ccd7a63a
Aleksi Romanov <aleksi.romanov@helsinki.fi>
2015-11-23 15:48:50 +0200

    MECE-329: Now the widget may use the version of jquery other then the host page does.

--- js/client.js
+++ js/client.js
@@ -1,7 +1,63 @@
+(function() {
+
+var jQuery;
+var JQUERY_VERSION = '1.4.2';
+
+// BEGING: Localize jQuery variable (http://alexmarandon.com/articles/web_widget_jquery/)
+
+(function() {
+
+    if (window.jQuery === undefined || window.jQuery.fn.jquery !== JQUERY_VERSION) {
+        var script_tag = document.createElement('script');
+        var jqueryUrl = "http://ajax.googleapis.com/ajax/libs/jquery/" + JQUERY_VERSION + "/jquery.min.js";
+        script_tag.setAttribute("type","text/javascript");
+        script_tag.setAttribute("src", jqueryUrl);
+        if (script_tag.readyState) {
+          script_tag.onreadystatechange = function () { // For old versions of IE
+              if (this.readyState == 'complete' || this.readyState == 'loaded') {
+                  onJQueryLoaded();
+              }
+          };
+        } else { // Other browsers
+          script_tag.onload = onJQueryLoaded;
+        }
+        // Try to find the head, otherwise default to the documentElement
+        (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+    } else {
+        // The jQuery version on the window is the one we want to use
+        jQuery = window.jQuery;
+    }
+
+    function onJQueryLoaded() {
+        console.log("onJQueryLoaded: BEGIN");
+        // Restore $ and window.jQuery to their previous values and store the
+        // new jQuery in our local jQuery variable
+        jQuery = window.jQuery.noConflict(true);
+        console.log("onJQueryLoaded:        jQuery.fn.jquery: " + jQuery.fn.jquery);
+        console.log("onJQueryLoaded:             $.fn.jquery: " + $.fn.jquery);
+        console.log("onJQueryLoaded: window.jQuery.fn.jquery: " + window.jQuery.fn.jquery);
+        testjQuery();
+    }
+
+    function testjQuery() {
+        var jsonp_url = "http://al.smeuh.org/cgi-bin/webwidget_tutorial.py?callback=?";
+        div = "#mece-content-div";
+        jQuery.getJSON(jsonp_url, function(data) {
+            jQuery(div).html("This data comes from another server: " + data.html);
+        });
+    }
+
+})();
+
+// END: Localize jQuery variable
+
 var meceNotifications = {};
 var notifications = [];
 
 meceNotifications.view = (function () {
+
+    console.log("meceNotifications.view: BEGIN");
+
     // elements containing notifications
     var contentDiv = document.getElementById("mece-content-div");
 
@@ -50,9 +106,13 @@ meceNotifications.view = (function () {
     //    });
     //}, 4000);
 
+    console.log("meceNotifications.view: END");
+
     return {
         parseNotifications: parseNotifications
     };
+
+    meceNotifications.view();
 
 })();
 
@@ -151,5 +211,7 @@ meceNotifications.client = (function () {
         setChannels: setChannels,
         start: start
     };
+})();
+
 })();
 
