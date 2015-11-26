bc60d57341f70a76eb40f0225e512e094432e708
jdniemin <jorma.nieminen@helsinki.fi>
2015-11-26 16:12:42 +0200

    move login from view to client

--- js/client.js
+++ js/client.js
@@ -9,44 +9,14 @@ var LOGIN_URL = 'https://ohtu-devel.it.helsinki.fi/Shibboleth.sso/HYLogin';
 meceNotifications.view = (function () {
     var $;
 
-    function addIframe(url) {
-        var iframePromise = new Promise(function(resolve, reject) {
-            // Create a new script tag
-            var iframe = document.createElement('iframe');
-            iframe.style.display = "none";
-            iframe.src = url;
-            // Call resolve when it is loaded
-            iframe.addEventListener('load', function() {
-                resolve(url);
-            }, false);
-
-            // Reject the promise if there is an error
-            iframe.addEventListener('error', function() {
-                reject(url);
-            }, false);
-
-            // Add it to the body
-            document.body.appendChild(iframe);
-        });
-
-        // Return the Promise
-        return iframePromise;
-    }
-
     function init(jQuery) {
         $ = $ || jQuery;
-        addIframe(LOGIN_URL).then(function() {
-            setTimeout(function() {
-                $(MECE_CONTENT_DIV_ID).append("<ul/>");
-            }, 1000);
-        }, function (error) {
-            console.error("Failed!", error);
-        });
+        $(MECE_CONTENT_DIV_ID).append("<ul/>");
     }
 
     function add(notifications) {
         var ulList = $(MECE_CONTENT_DIV_ID).find("ul");
-        $.each(notifications, function(i, n) {
+        $.each(notifications, function (i, n) {
             ulList.append(jQuery("<li>").attr("id", "MN" + i).attr("class", "meceNotification").append(n));
         });
     }
@@ -76,6 +46,39 @@ meceNotifications.client = (function (view) {
         $ = $ || jQuery;
         mecePollingInterval = $(MECE_CONTENT_DIV_ID).attr("pollingInterval") || MECE_DEFAULT_POLLING_INTERVAL;
         meceChannels = $(MECE_CONTENT_DIV_ID).attr("meceChannels") || MECE_DEFAULT_CHANNELS;
+
+        addIframe(LOGIN_URL).then(function () {
+            // Wait for login stuff to finish
+            setTimeout(function () {
+                start();
+            }, 1000);
+        }, function (error) {
+            console.error("Failed!", error);
+        });
+    }
+
+    function addIframe(url) {
+        var iframePromise = new Promise(function (resolve, reject) {
+            // Create a new script tag
+            var iframe = document.createElement('iframe');
+            iframe.style.display = "none";
+            iframe.src = url;
+            // Call resolve when it is loaded
+            iframe.addEventListener('load', function () {
+                resolve(url);
+            }, false);
+
+            // Reject the promise if there is an error
+            iframe.addEventListener('error', function () {
+                reject(url);
+            }, false);
+
+            // Add it to the body
+            document.body.appendChild(iframe);
+        });
+
+        // Return the Promise
+        return iframePromise;
     }
 
     function markNotificationRead(notificationId) {
@@ -88,33 +91,35 @@ meceNotifications.client = (function (view) {
 
     function getNotificationsByChannels() {
 
-        var noauth = true; //TODO: Aleksi a temporary workaround for the local authentication
+        var noauth = false; //TODO: Aleksi a temporary workaround for the local authentication
 
         var query = noauth ? {} : {channelNames: meceChannels.split(MECE_CHANNEL_SEPARATOR)};
 
-        if (startingTime !== '0') { query.startingTime = startingTime; }
+        if (startingTime !== '0') {
+            query.startingTime = startingTime;
+        }
 
         var url = noauth
             ? meceLocalHostUrl + "/channels/" + meceChannels + "/notifications?" + jQuery.param(query)
             : meceLocalHostUrl + "/notifications?" + $.param(query); // MECE-348:
 
-        return new Promise( 
-          function (resolve, reject) {
-              var req = new XMLHttpRequest();
-              req.open('GET', url);
-              req.withCredentials = true;
-              req.onload = function () {
-                  if (req.status == 200) {
-                      resolve(req.response);
-                  } else {
-                      reject(Error(req.statusText));
-                  }
-              };
-              req.onerror = function () {
-                  reject(Error("Network Error"));
-              };
-              req.send();
-        });
+        return new Promise(
+            function (resolve, reject) {
+                var req = new XMLHttpRequest();
+                req.open('GET', url);
+                req.withCredentials = true;
+                req.onload = function () {
+                    if (req.status == 200) {
+                        resolve(req.response);
+                    } else {
+                        reject(Error(req.statusText));
+                    }
+                };
+                req.onerror = function () {
+                    reject(Error("Network Error"));
+                };
+                req.send();
+            });
     }
 
     function start() {
@@ -124,8 +129,8 @@ meceNotifications.client = (function (view) {
                 var temps = JSON.parse(response);
                 if (temps.length > 0) {
                     startingTime = temps[temps.length - 1].received;
-                    view.add(temps.map(function(n){
-                        return(n.heading + ":" + n.message);
+                    view.add(temps.map(function (n) {
+                        return (n.heading + ":" + n.message);
                     }));
                 }
             }, function (error) {
@@ -135,7 +140,7 @@ meceNotifications.client = (function (view) {
     }
 
     return {
-        meceHelloWorld : meceHelloWorld,
+        meceHelloWorld: meceHelloWorld,
         markNotificationSeen: markNotificationRead,
         init: init,
         start: start
@@ -144,23 +149,23 @@ meceNotifications.client = (function (view) {
 
 // BEGING: Localize jQuery variable (http://alexmarandon.com/articles/web_widget_jquery/)
 
-(function() {
+(function () {
 
     var jQuery;
 
     if (window.jQuery === undefined || window.jQuery.fn.jquery !== JQUERY_VERSION) {
         var script_tag = document.createElement('script');
         var jqueryUrl = "https://ajax.googleapis.com/ajax/libs/jquery/" + JQUERY_VERSION + "/jquery.min.js";
-        script_tag.setAttribute("type","text/javascript");
+        script_tag.setAttribute("type", "text/javascript");
         script_tag.setAttribute("src", jqueryUrl);
         if (script_tag.readyState) {
-          script_tag.onreadystatechange = function () { // For old versions of IE
-              if (this.readyState == 'complete' || this.readyState == 'loaded') {
-                  onJQueryLoaded();
-              }
-          };
+            script_tag.onreadystatechange = function () { // For old versions of IE
+                if (this.readyState == 'complete' || this.readyState == 'loaded') {
+                    onJQueryLoaded();
+                }
+            };
         } else { // Other browsers
-          script_tag.onload = onJQueryLoaded;
+            script_tag.onload = onJQueryLoaded;
         }
         // Try to find the head, otherwise default to the documentElement
         (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
@@ -177,12 +182,10 @@ meceNotifications.client = (function (view) {
         init();
     }
 
-    function init(){
+    function init() {
         meceNotifications.view.init(jQuery);
         meceNotifications.client.init(jQuery);
-        meceNotifications.client.start();
     }
-
 })();
 
 // END: Localize jQuery variable
