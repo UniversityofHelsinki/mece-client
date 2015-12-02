686c5060213370b00153dc40f6af723bd00d8e45
Toni Rasanen <toni.rasanen@helsinki.fi>
2015-12-02 10:25:10 +0200

    MECE-366. Viestilistan näyttäminen. Kuvan mukaisia muutoksia.

--- js/view.js
+++ js/view.js
@@ -3,33 +3,45 @@ var meceNotifications = (function (mece) {
     function __initWidgetList() {
         var ul = $("<ul/>")
             .addClass("mece_list")
-            .css("background-color", "WhiteSmoke")
+            .css("background-color", "#FFF")
             .css("color", "black");
         $(mece.contentDivId)
+            .css("font-size", ".8em")
+            .css("font-family", "Open Sans,Helvetica,Arial,sans-serif,Nimbus Sans L")
             .css("display", "none")
-            .css("width", "500px")
-            .css("height", "350px")
+            .css("width", "320px")
+            .css("height", "330px")
             .css("overflow", "auto")
-            //.css("margin", "auto")
-            .css("border", "3px solid navy");
+            .css("background-color", "#FFF")
+            .css("border", "0px");
         $(mece.contentDivId).append(ul);
         $(mece.iconId).css("border", "3px solid navy")
             .css("height", "64px")
-            .css("background", "#e74c3c")
+            .css("background", "#FFF")
             .css("padding", "0px 32px")
             .css("border-bottom", "1px solid #C24032")
             .css("text-align", "left")
             .css("box-shadow", "0px 0px 4px #C24032");
     }
 
+    // one notification inside li-element
+    // content:
+    //    notification heading
+    //    notification message
+    //    notification link
+    //    something else?
+    //
     function __addWidgetIteminitWidget(offset, notification) {
+        console.log("notification: " + notification);
         var ulList = $(mece.contentDivId).find("ul");
         var link = $("<a>").attr("href", notification[0]).text(notification[0]);
+            link.css("color", "#005479")
+            .css("text-decoration", "none");
         var li = $("<li>").attr("id", "MN" + offset)
             .css("list-style-image", "url('images/" + (offset % 3 > 0 ? "car" : "photo") + ".png')")
             .css("height", "100px")
-            //.css("display", "inline-block")
-            .css("border-bottom", "1px solid #000")
+            .css("border-bottom", "2px solid #f5f5f5")
+            .css("background-color", "#FFF")
             .append(link);
         ulList.append(li);
     }
