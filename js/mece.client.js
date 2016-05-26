bf8beb3455858c8872fc692b8f53ddbb20048899
vmheikki <veli-matti.heikkila@helsinki.fi>
2016-05-26 23:21:37 +0300

    MECE-480

--- js/mece.client.js
+++ js/mece.client.js
@@ -60,7 +60,6 @@
 
     function init() {
         initializerStuff();
-        checkStyleSheet();
         readAndInitializeAttributeValues();
         initWidgetList();
         dialog();
@@ -319,30 +318,6 @@
         return translations[key][myLanguage || language];
     }
 
-    function checkStyleSheet() {
-
-        var found = false;
-        for(var i = 0; i < document.styleSheets.length; i++) {
-            var href = document.styleSheets[i].href;
-            if(href) {
-                if(href === FONT_AWESOME_URL) {
-                    found=true;
-                    break;
-                }
-                if (href.split('/').pop() === 'font-awesome.css' || href.split('/').pop() === 'font-awesome.min.css') {
-                    found=true;
-                    break;
-                }
-            }
-        }
-
-        if(!found){
-            $('head').append(
-                $('<link rel="stylesheet" type="text/css" href="' + FONT_AWESOME_URL + '" />')
-            );
-        }
-    }
-
     function resizeWidget() {
 
         var widgetWidth = meceConfig.windowWidth,
@@ -546,6 +521,9 @@
 
     function dialog() {
         //$(iconDivId).append($("<img>").attr("src", IMAGES_URI + "/bell.png").text("bell image"));
+        $(iconDivId).append('<svg width="20" height="20" viewBox="0 0 1792 1792" fill="#178feb" xmlns="http://www.w3.org/2000/svg"><path d="M912 1696q0-16-16-16-59 0-101.5-42.5t-42.5-101.5q0-16-16-16t-16 16q0 73 51.5 124.5t124.5 51.5q16 0 16-16zm816-288q0 52-38 90t-90 38h-448q0 106-75 181t-181 75-181-75-75-181h-448q-52 0-90-38t-38-90q50-42 91-88t85-119.5 74.5-158.5 50-206 19.5-260q0-152 117-282.5t307-158.5q-8-19-8-39 0-40 28-68t68-28 68 28 28 68q0 20-8 39 190 28 307 158.5t117 282.5q0 139 19.5 260t50 206 74.5 158.5 85 119.5 91 88z"/></svg>');
+
+
         $(iconDivId).click(function (e) {
             e.stopPropagation();
             if ($(this).hasClass("active")) {
